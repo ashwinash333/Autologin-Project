cbb3ec6a6b9b2308455d8d70b52bd3fb75c87a37
ashwin macha <machaashwinash33@gmail.com>
2017-07-04 13:47:20 +0530

    adding files
    adding project files

---
+++ Autologin/Autologin/Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.clipcop = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Modern No. 20", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(60, 6)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 15)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "clipboard text"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(407, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Togglepass"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 18
+        Me.ListBox1.Location = New System.Drawing.Point(3, 3)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(158, 328)
+        Me.ListBox1.TabIndex = 1
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 3
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.14423!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.85577!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 160.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.ListBox3, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.ListBox1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.ListBox2, 1, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(39, 72)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(569, 337)
+        Me.TableLayoutPanel1.TabIndex = 12
+        '
+        'ListBox3
+        '
+        Me.ListBox3.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 18
+        Me.ListBox3.Location = New System.Drawing.Point(411, 3)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(146, 328)
+        Me.ListBox3.TabIndex = 3
+        '
+        'ListBox2
+        '
+        Me.ListBox2.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 18
+        Me.ListBox2.Location = New System.Drawing.Point(167, 3)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(238, 328)
+        Me.ListBox2.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(621, 2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 19
+        Me.Button3.Text = "openfile"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(489, 2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(98, 23)
+        Me.Button2.TabIndex = 18
+        Me.Button2.Text = "Openinfirefox"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'clipcop
+        '
+        Me.clipcop.Enabled = False
+        Me.clipcop.Location = New System.Drawing.Point(154, 4)
+        Me.clipcop.Name = "clipcop"
+        Me.clipcop.Size = New System.Drawing.Size(221, 20)
+        Me.clipcop.TabIndex = 16
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("DejaVu Serif Condensed", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(468, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(93, 22)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "Password"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("DejaVu Serif Condensed", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(260, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(108, 24)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Username"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("DejaVu Serif Condensed", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(60, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(121, 24)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Description"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(621, 37)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 20
+        Me.Button4.Text = "Reload"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(734, 411)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.clipcop)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Name = "Form1"
+        Me.Text = "Autologin"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents clipcop As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Autologin/Autologin/Form1.vb
@@ -0,0 +1,253 @@
+﻿Imports System.IO
+Imports System.Web.Script.Serialization
+
+
+Public Class Form1
+    Dim maskproperty As Boolean = True
+    Dim s As JavaScriptSerializer = New JavaScriptSerializer()
+    Dim j As String = File.ReadAllText(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\json1.json")
+    Dim l As login = New login()
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox3.Items.Clear()
+
+        If maskproperty = True Then
+
+            l = s.Deserialize(Of login)(j)
+            Dim listcount As Integer = l.login.Count
+            For i = 0 To listcount - 1
+                ListBox3.Items.Add(l.login.Item(i).password.ToString)
+            Next i
+            maskproperty = False
+            Return
+        ElseIf maskproperty = False Then
+
+
+            l = s.Deserialize(Of login)(j)
+            Dim listcount As Integer = l.login.Count
+            Dim i As Integer
+            For i = 0 To listcount - 1
+                Dim strC As String
+                Dim setcount As Integer = l.login.Item(i).password.ToString.Count
+                strC = StrDup(setcount, "*")
+                ListBox3.Items.Add(strC)
+            Next i
+            maskproperty = True
+            Return
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        l = s.Deserialize(Of login)(j)
+        Dim listcount As Integer = l.login.Count
+        Dim i As Integer
+
+        For i = 0 To listcount - 1
+            Dim strC As String
+            Dim setcount As Integer = l.login.Item(i).password.ToString.Count
+            ListBox1.Items.Add(l.login.Item(i).website.ToString)
+            ListBox2.Items.Add(l.login.Item(i).username.ToString)
+            strC = StrDup(setcount, "*")
+            ListBox3.Items.Add(strC)
+        Next i
+
+    End Sub
+
+
+
+    Private Sub onlistitemclicked(sender As Object, e As EventArgs) Handles ListBox1.DoubleClick
+
+        l = s.Deserialize(Of login)(j)
+        Dim indf As Integer = ListBox1.SelectedIndex
+        Dim username As String = l.login.Item(indf).username
+        Dim password As String = l.login.Item(indf).password
+        Dim url As String = l.login.Item(indf).url
+        Dim wsh As Object = CreateObject("WScript.Shell")
+        If url.Equals("https://accounts.google.com/signin") Then
+            wsh.Run("chrome.exe", 3)
+            Threading.Thread.Sleep(5000)
+            wsh.SendKeys("^+n")
+            Threading.Thread.Sleep(3000)
+            wsh.SendKeys(url)
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{ENTER}")
+            Threading.Thread.Sleep(8000)
+            wsh.SendKeys(username)
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{ENTER}")
+            Threading.Thread.Sleep(4000)
+            wsh.SendKeys(password)
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{ENTER}")
+            Threading.Thread.Sleep(4000)
+            wsh.SendKeys("^l")
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{BKSP}")
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("https://mail.google.com/mail/u/0/#inbox")
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{ENTER}")
+            Threading.Thread.Sleep(4000)
+        End If
+        If url.Equals("https://www.facebook.com/?_rdr") Then
+            wsh.Run("chrome.exe", 3)
+            Threading.Thread.Sleep(5000)
+            wsh.SendKeys("^+n")
+            Threading.Thread.Sleep(3000)
+            wsh.SendKeys(url)
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{ENTER}")
+            Threading.Thread.Sleep(6000)
+            wsh.sendkeys("+{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("+{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("+{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("+{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("+{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys(username)
+            Threading.Thread.Sleep(1000)
+            wsh.sendkeys("{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys(password)
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("{ENTER}")
+            Threading.Thread.Sleep(4000)
+        End If
+
+    End Sub
+
+
+
+    Private Sub onlist3click(sender As Object, e As EventArgs) Handles ListBox3.DoubleClick
+        Dim setthis As Integer = ListBox3.SelectedIndex
+
+        l = s.Deserialize(Of login)(j)
+        My.Computer.Clipboard.SetText(l.login.Item(setthis).password.ToString)
+
+        clipcop.Text = ListBox3.SelectedItem.ToString()
+    End Sub
+
+    Private Sub onlist2click(sender As Object, e As EventArgs) Handles ListBox2.DoubleClick
+        My.Computer.Clipboard.SetText(ListBox2.SelectedItem.ToString())
+        clipcop.Text = ListBox2.SelectedItem.ToString()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        l = s.Deserialize(Of login)(j)
+        Dim indf As Integer = ListBox1.SelectedIndex
+        Dim username As String = l.login.Item(indf).username
+        Dim password As String = l.login.Item(indf).password
+        Dim url As String = l.login.Item(indf).url
+        Dim wsh As Object = CreateObject("WScript.Shell")
+        If url.Equals("https://accounts.google.com/signin") Then
+            wsh.Run("firefox.exe", 3)
+            Threading.Thread.Sleep(5000)
+            wsh.SendKeys("^+p")
+            Threading.Thread.Sleep(3000)
+            wsh.SendKeys(url)
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{ENTER}")
+            Threading.Thread.Sleep(8000)
+            wsh.SendKeys(username)
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{ENTER}")
+            Threading.Thread.Sleep(4000)
+            wsh.SendKeys(password)
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{ENTER}")
+            Threading.Thread.Sleep(4000)
+            wsh.SendKeys("^l")
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{BKSP}")
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("https://mail.google.com/mail/u/0/#inbox")
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{ENTER}")
+            Threading.Thread.Sleep(4000)
+        End If
+        If url.Equals("https://www.facebook.com/?_rdr") Then
+            wsh.Run("firefox.exe", 3)
+            Threading.Thread.Sleep(5000)
+            wsh.SendKeys("^+p")
+            Threading.Thread.Sleep(3000)
+            wsh.SendKeys(url)
+            Threading.Thread.Sleep(1000)
+            wsh.SendKeys("{ENTER}")
+            Threading.Thread.Sleep(6000)
+            wsh.sendkeys("+{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("+{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("+{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("+{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("+{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys(username)
+            Threading.Thread.Sleep(1000)
+            wsh.sendkeys("{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys(password)
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("{TAB}")
+            Threading.Thread.Sleep(600)
+            wsh.sendkeys("{ENTER}")
+            Threading.Thread.Sleep(4000)
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        OpenFileDialog1.Filter = "(*json)|*.json"
+        If Directory.Exists(My.Computer.FileSystem.SpecialDirectories.MyDocuments) Then
+            OpenFileDialog1.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        End If
+        OpenFileDialog1.ShowDialog()
+
+        Shell("Notepad " + OpenFileDialog1.FileName, vbNormalFocus)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+        ListBox3.Items.Clear()
+        Dim s As JavaScriptSerializer = New JavaScriptSerializer()
+        Dim j As String = File.ReadAllText(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\json1.json")
+        Dim l As login = New login()
+        l = s.Deserialize(Of login)(j)
+        Dim listcount As Integer = l.login.Count
+        For i = 0 To listcount - 1
+            Dim strC As String
+            Dim setcount As Integer = l.login.Item(i).password.ToString.Count
+            ListBox1.Items.Add(l.login.Item(i).website.ToString)
+            ListBox2.Items.Add(l.login.Item(i).username.ToString)
+            strC = StrDup(setcount, "*")
+            ListBox3.Items.Add(strC)
+        Next i
+    End Sub
+End Class
+
+Class login
+    Public Property login As List(Of loginDetails)
+End Class
+
+Class loginDetails
+
+    Public Property website As String
+    Public Property username As String
+    Public Property password As String
+    Public Property url As String
+End Class
+
